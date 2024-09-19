5abe3de12cb7fb73d71db73d627a2ad76d45ef09
labutog4321 <158170649+labutog4321@users.noreply.github.com>
2024-09-19 16:18:37 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -37,7 +37,7 @@ module.exports.run = async function ({ api, event, args }) {
             const generatedText = response.data.response;
 
             // Ai Answer Here
-            api.sendMessage(`${generatedText}`, event.threadID, messageID);
+            api.sendMessage(`•| 𝙱𝙾𝙶𝙰𝚁𝚃 𝙰𝙸 𝙱𝙾𝚃 |•\n\n${generatedText}`, event.threadID, messageID);
         } else {
             console.error('API response did not contain expected data:', response.data);
             api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Response data: ${JSON.stringify(response.data)}`, event.threadID, messageID);
